c0ed12d5ab7fb0ef5b9cba10e6e09e4a909e0660
KoreanFubuki <145092552+KoreanFubuki@users.noreply.github.com>
2023-09-19 16:51:18 +0400

    Add project files.

---
+++ Exercise 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the Temperature?");
+            string ttemp = Console.ReadLine();
+            int temp;
+            int.TryParse(ttemp, out temp);
+            string answer;
+            if (int.TryParse(ttemp, out temp))
+            {
+                answer = temp <= 15 ? "it is too cold here" : 16 <= temp && temp <= 28 ? "it is okay" : "it is hot here";
+                Console.WriteLine(answer);
+            }
+            else
+            {
+                Console.WriteLine("Not a valid Temperature");
+            }
+            Console.ReadKey();  
+        }
+    }
+}
